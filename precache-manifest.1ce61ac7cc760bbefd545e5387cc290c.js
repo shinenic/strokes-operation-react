a5b21a2992884732b25896be883e67f7c2488f13
Kadenz <galadiya41@gmail.com>
2019-09-25 01:04:00 +0800

    Updates

---
+++ precache-manifest.1ce61ac7cc760bbefd545e5387cc290c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0c4c8920db78ba6071fd006e244c8a",
+    "url": "/strokes-operation-react/index.html"
+  },
+  {
+    "revision": "606f209c7941ad9d259c",
+    "url": "/strokes-operation-react/static/css/main.a9ac2f10.chunk.css"
+  },
+  {
+    "revision": "d443cee304cbfdd9c627",
+    "url": "/strokes-operation-react/static/js/2.421a1c82.chunk.js"
+  },
+  {
+    "revision": "606f209c7941ad9d259c",
+    "url": "/strokes-operation-react/static/js/main.b6ba9f0d.chunk.js"
+  },
+  {
+    "revision": "9ac17d03d60e490530b2",
+    "url": "/strokes-operation-react/static/js/runtime~main.d942dcce.js"
+  },
+  {
+    "revision": "7e7861afc7e16db34a529d7251ba059c",
+    "url": "/strokes-operation-react/static/media/cat.7e7861af.png"
+  }
+]);

dd2ebab8ca719ba187aada46dcb9a57c849660e9
Kadenz <galadiya41@gmail.com>
2019-09-25 15:54:48 +0800

    調整card內容成grid排版

--- src/components/IndexCard.js
+++ src/components/IndexCard.js
@@ -8,6 +8,9 @@ import CountUp from 'react-countup';
 import { connect } from 'react-redux';
 import { cleanAllInput } from '../actions';
 
+const CardThemeColor = ['#DB6623', '#3E5EB3', '#AAC634'];
+const CardImg = [BooksImg, checkImg, linkImg];
+
 const IndexCardDiv = styled.div`
   height:100%;
   width:100%;
@@ -18,17 +21,16 @@ const GridContainer = styled.div`
   height:100%;
   width:100%;
   display:grid;
-  grid-template-columns:40px 0.33fr 0.33fr 0.33fr 40px;
+  grid-template-columns:80px 0.33fr 0.33fr 0.33fr 160px;
   grid-template-rows:0.3fr 400px 0.7fr;
   grid-template-areas:
     ". . . . ."
-    ". card1 card2 card3 ."
+    ". card0 card1 card2 ."
     ". . . . .";
 `;
 
-
-const Books = styled.img`
-  content:url(${BooksImg});
+const Image = styled.img`
+  content:url(${props => CardImg[props.cardIndex]});
   height:67%;
   width:67%;
   filter:invert(1);
@@ -37,41 +39,37 @@ const Books = styled.img`
   top:50%;
   transform: translate(-50%,-50%);
 `;
-const Check = styled(Books)`
-  content:url(${checkImg});
-`;
-const Link = styled(Books)`
-  content:url(${linkImg});
-`;
 
-
-const BooksBorder = styled.div`
+const ImgBorder = styled.div`
   border-radius:50%; 
   /* border: 3px solid rgb(215,225,235);  */
   transition: 0.3s;
-  background:rgb(219,102,35);
+  background:${props => CardThemeColor[props.cardIndex]};
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2); 
   height:64px;
   width:64px;
   margin: 60px auto 60px auto;
   cursor: pointer;
   position:relative;
-`;
-const CheckBorder = styled(BooksBorder)`
-  background:rgb(62,94,179);
-
-`;
-const LinkBorder = styled(BooksBorder)`
-  background:rgb(170,198,52);
-
+  display: grid;
+  justify-content: center;
+  align-items: center;
 `;
 const CountText = styled.span`
+  grid-area:data;
   font-weight:bold;
   font-size:26px;
+  display: grid;
+  align-items: start;
+`;
+const TitleText = styled.h2`
+  grid-area:title;
+  display: grid;
+  align-items: start;
 `;
 
-const Card1 = styled.div`
-  grid-area:card1;
+const Card = styled.div`
+  grid-area:${props => 'card' + props.cardIndex};
   box-shadow: 0 2px 4px 0 rgba(0,0,0,0.1); 
   border-radius:5px;
   transition: 0.3s;
@@ -79,37 +77,32 @@ const Card1 = styled.div`
   max-width:250px;
   height:95%;
   box-sizing:border-box;
+  margin:0px auto;
   padding:5px;
   text-align:center;
-  animation: ${ShowDivAni} 1s 1 both 0.1s;
+  animation: ${ShowDivAni} 0.5s 1 both ${props => (Number(props.cardIndex) * 0.2 + 0.1).toString() + 's'};
   background:rgb(217,225,232);
-  box-sizing: border-box;
   &:hover{
     height:100%;
-    border-bottom:10px solid rgb(219,102,35);
+    border-bottom:10px solid ${props => CardThemeColor[props.cardIndex]};
     box-shadow: 0 4px 6px 0 rgba(0,0,0,0.2); 
   }
-  &:hover ${BooksBorder} {
+  &:hover ${ImgBorder} {
     height:80px;
     width:80px;
   }
 `;
-const Card2 = styled(Card1)`
-  grid-area:card2;
-  animation: ${ShowDivAni} 1s 1 both 0.3s;
-  &:hover{
-    border-bottom:10px solid rgb(62,94,179);
-  }
-`;
-const Card3 = styled(Card1)`
-  grid-area:card3;
-  animation: ${ShowDivAni} 1s 1 both 0.5s;
-  &:hover{
-    border-bottom:10px solid rgb(170,198,52);
-  }
-`;
 
-
+const CardGridContent = styled.div`
+  height:100%;
+  width:100%;
+  display:grid;
+  grid-template-rows:0.5fr 0.2fr 0.3fr;
+  grid-template-areas:
+    "img"
+    "title"
+    "data";
+`;
 
 class IndexCard extends PureComponent {
   render() {
@@ -121,43 +114,46 @@ class IndexCard extends PureComponent {
     return (
       <IndexCardDiv>
         <GridContainer>
-          {/* <Blank /> */}
-          <Card1>
-            <div>
-              <BooksBorder><Books /></BooksBorder>
-              <h2>單字庫</h2>
+
+          <Card cardIndex={0}>
+            <CardGridContent>
+              <ImgBorder cardIndex={0}><Image cardIndex={0} /></ImgBorder>
+              <TitleText>單字庫</TitleText>
               <CountText>
                 <CountUp
                   separator=","
                   suffix="  字"
                   end={Object.keys(this.props.character).length} />
               </CountText>
-            </div>
-          </Card1>
-          <Card2>
-            <div>
-              <CheckBorder><Check /></CheckBorder>
-              <h2>已選擇組合</h2>
+            </CardGridContent>
+          </Card>
+
+          <Card cardIndex={1}>
+            <CardGridContent>
+              <ImgBorder cardIndex={1}><Image cardIndex={1} /></ImgBorder>
+              <TitleText>已選擇組合</TitleText>
               <CountText>
                 <CountUp
                   separator=","
                   suffix="  組"
                   end={pickedCount(this.props.pickedComb)} />
               </CountText>
-            </div>
-          </Card2>
-          <Card3>
-            <div>
-              <LinkBorder><Link /></LinkBorder>
-              <h2>總組合數</h2>
+            </CardGridContent>
+          </Card>
+
+          <Card cardIndex={2}>
+            <CardGridContent>
+              <ImgBorder cardIndex={2}><Image cardIndex={2} /></ImgBorder>
+              <TitleText>總組合數</TitleText>
               <CountText>
                 <CountUp
                   separator=","
                   suffix="  組"
                   end={Object.keys(this.props.character).length * Object.keys(this.props.character).length} />
               </CountText>
-            </div>
-          </Card3>
+            </CardGridContent>
+          </Card>
+
         </GridContainer>
       </IndexCardDiv >
     )

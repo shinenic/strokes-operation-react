72ba498f885f7060ca704acce0e5d887041d2844
Kadenz <galadiya41@gmail.com>
2019-09-26 00:14:47 +0800

    Updates

--- precache-manifest.0d656b4a5e66eaa9086d30ff25b89e9a.js
+++ precache-manifest.0d656b4a5e66eaa9086d30ff25b89e9a.js
@@ -1,19 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ac0054676b88bf24aff6f00a6359ca85",
+    "revision": "e49932631c0f3cd48d739b27491ce576",
     "url": "/strokes-operation-react/index.html"
   },
   {
-    "revision": "e17dcb232862c6254ea4",
+    "revision": "cb65718e018de2686a3a",
+    "url": "/strokes-operation-react/static/css/2.19b13f22.chunk.css"
+  },
+  {
+    "revision": "ecd052c18e0d20715aab",
     "url": "/strokes-operation-react/static/css/main.89409cee.chunk.css"
   },
   {
-    "revision": "4989bbd6b354dc00328e",
-    "url": "/strokes-operation-react/static/js/2.95c97cc3.chunk.js"
+    "revision": "cb65718e018de2686a3a",
+    "url": "/strokes-operation-react/static/js/2.3373b23d.chunk.js"
   },
   {
-    "revision": "e17dcb232862c6254ea4",
-    "url": "/strokes-operation-react/static/js/main.077c7abf.chunk.js"
+    "revision": "ecd052c18e0d20715aab",
+    "url": "/strokes-operation-react/static/js/main.8184ea0a.chunk.js"
   },
   {
     "revision": "9ac17d03d60e490530b2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/strokes-operation-react/precache-manifest.c2d44554b7703d0a941261516b163ad0.js"
+  "/strokes-operation-react/precache-manifest.0d656b4a5e66eaa9086d30ff25b89e9a.js"
 );
 
 self.addEventListener('message', (event) => {
